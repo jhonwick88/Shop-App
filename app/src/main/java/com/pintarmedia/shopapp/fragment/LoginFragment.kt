2f6174cc47b3e5453a7b46cd705af57cd73374db
Yuwono <labspintar@gmail.com>
2021-10-04 09:40:28 +0700

    add login fragment

--- app/src/main/java/com/pintarmedia/shopapp/fragment/LoginFragment.kt
+++ app/src/main/java/com/pintarmedia/shopapp/fragment/LoginFragment.kt
@@ -1,10 +1,15 @@
 package com.pintarmedia.shopapp.fragment
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.pintarmedia.shopapp.R
+import kotlinx.android.synthetic.main.login_fragment.*
+import kotlinx.android.synthetic.main.login_fragment.view.*
 
 class LoginFragment: Fragment() {
     override fun onCreateView(
@@ -12,6 +17,25 @@ class LoginFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val vie = inflater.inflate(R.layout.login_fragment, container,false)
+        vie.next_button.setOnClickListener {
+            if(!isValidPassword(password_edit_text.text)){
+                password_text_input.error = getString(R.string.shr_error_password)
+            }else{
+                password_text_input.error = null
+                Toast.makeText(it.context, "Ok next", Toast.LENGTH_SHORT).show()
+            }
+            vie.password_edit_text.setOnKeyListener { _, _, _ ->
+                if (isValidPassword(password_edit_text.text)){
+                    password_text_input.error = null
+                }
+                false
+            }
+        }
+        return vie
+    }
+
+    private fun isValidPassword(text: Editable?): Boolean {
+        return text!=null && text.length >= 8
     }
 }
